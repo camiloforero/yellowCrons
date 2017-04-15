be8a5defd07e327d5eda688a82e96c4c2dff2eaa
Camilo Forero <ce.forero2551@uniandes.edu.co>
2017-04-14 21:04:44 -0500

    modificados los scripts actuales

--- andes_scripts.py
+++ andes_scripts.py
@@ -31,16 +31,29 @@ def andes_daily_load():
         print e
 
     try:
-        print "Cargando Approved de ICX"
-        approved_igcdp_apps = ex_api.get_past_interactions('approved', 1, 1395, False, program='icx')
+        print "Cargando Approved de IGV"
+        approved_igv_apps = ex_api.get_past_interactions('approved', 1, 1395, False, program='igv')
         #approved_igcdp_apps = ex_api.get_interactions('approved', 1395, program='icx', start_date='2016-01-01', end_date='2016-11-29')
-        load_approved_icx_eps(approved_igcdp_apps)
+        load_approved_igv_trainees(approved_igv_apps)
         print "Los EPs aprobados de ICX fueron cargados exitosamente"
     except (expaApi.APIUnavailableException) as e:
         print "Error cargando los trainees aprobados de ICX"
         print e
     except Exception as e:
         print "Unknown error - ICX approved apps"
+        print e
+
+    try:
+        print "Cargando Opens de IGV"
+        applied_igv_apps = ex_api.get_past_interactions('applied', 1, 1395, False, program='igv')
+        #approved_igcdp_apps = ex_api.get_interactions('approved', 1395, program='icx', start_date='2016-01-01', end_date='2016-11-29')
+        load_applied_igv_trainees(applied_igv_apps)
+        print "Los EPs aprobados de ICX fueron cargados exitosamente"
+    except (expaApi.APIUnavailableException) as e:
+        print "Error cargando los trainees aprobados de ICX"
+        print e
+    except Exception as e:
+        print "Unknown error - ICX applied apps"
         print e
 #    try:
 #        udea_opens = ex_api.get_past_interactions('registered', 1, 1746, False, program='icx')
@@ -110,23 +123,46 @@ def load_andes_opens(andes_opens):
         p_api.create_item({'fields':attributes})
         print "Se ha agregado a %s al espacio de OGX de opens" % person['email']
 
-def load_approved_apps(approved_apps):
-    for app in approved_apps['items']:
-        p_api = api.PodioApi(15998856)
+def load_approved_igv_trainees(apps):
+    p_api = api.PodioApi(18275738)
+    for app in apps['items']:
         attributes = {
-            123889165:app['person']['first_name'],
-            123889166:app['person']['last_name'],
-            123889167:app['person']['email'],
-            123889168:app['opportunity']['programmes'][0]['short_name'],
-            123889169:unicode(app['person']['id']),
-            123889170:unicode(app['opportunity']['id']),
-            123889171:app['opportunity']['office']['name'],
-            124789464:app['opportunity']['title'], #Nombre del proyecto
-            129666024:{'start_date':app['an_signed_at'].split('T')[0]}#Fecha del match
+            143812438:app['person']['first_name'],
+            143812439:app['person']['last_name'],
+            143812440:app['person']['email'],
+            143812442:unicode(app['person']['id']),
+            143812444:unicode(app['opportunity']['id']),
+            143812445:app['opportunity']['office']['name'],
+            143815324:app['id'],
+            143812446:app['person']['home_lc']['country'], #País origen
+            143812443:app['opportunity']['title'], #Nombre del proyecto
             }
         try:
             p_api.create_item({'fields':attributes})
-            print "Se ha agregado a %s al espacio de OGX de EPs aprobados" % app['person']['email']
+            print "Se ha agregado a %s al espacio de IGV Strike de Trainees aprobados" % app['person']['email']
+
+        except api.api.transport.TransportException as te:
+            print "Transport exception en %s" % app['person']['email']
+            print app
+            print te
+
+def load_applied_igv_trainees(apps):
+    p_api = api.PodioApi(18275836)
+    for app in apps['items']:
+        attributes = {
+            143813278:app['person']['first_name'],
+            143813279:app['person']['last_name'],
+            143813280:app['person']['email'],
+            143813282:unicode(app['person']['id']),
+            143813283:app['opportunity']['title'], #Nombre del proyecto
+            143813284:unicode(app['opportunity']['id']),
+            143813638:app['id'],
+            143813285:app['opportunity']['office']['name'],
+            143813286:app['person']['home_lc']['country'], #País origen
+            }
+        try:
+            p_api.create_item({'fields':attributes})
+            print "Se ha agregado a %s al espacio de IGV Strike de Trainees aprobados" % app['person']['email']
 
         except api.api.transport.TransportException as te:
             print "Transport exception en %s" % app['person']['email']
@@ -137,19 +173,6 @@ def email_new_visitors(new_visitors):
     email = mailApi.MailApi('correo_nuevos_inscritos')
 
 
-def load_approved_icx_eps(applications):
-    p_api = api.PodioApi(15886643)
-    for app in applications['items']:
-        attributes = {
-            122922712:app['person']['first_name'],
-            122923595:app['person']['last_name'],
-            122923598:app['person']['email'],
-            122923596:app['opportunity']['programmes'][0]['short_name'], #Programa
-            122923597:unicode(app['person']['id']), #Trainee EXPA ID
-            122923599:app['opportunity']['title'], #Nombre del proyecto
-            122923600:unicode(app['opportunity']['id']), #Nombre del trainee
-            122923601:app['person']['home_lc']['name'], #LC origen
-            122923604:app['person']['home_lc']['country'], #País origen
             122923603:{'start_date':app['an_signed_at'].split('T')[0]}
             }
         try:
